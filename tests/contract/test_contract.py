974214dc6a47500f9a514fd4f2a6c226a9a3c538
Amund Tenstad <github@amund.io>
2022-09-22 09:53:59 +0200

    feat: query graph at specific timestamp (#11)
    * feat: query graph at specific timestamp
    
    * fix: finally fix the contract tests
    
    * tweak: variable name

--- tests/contract/test_contract.py
+++ tests/contract/test_contract.py
@@ -1,6 +1,7 @@
 """Contract tests."""
 
 from textwrap import dedent
+import time
 
 import pytest
 
@@ -65,12 +66,62 @@ def test_delete_graph(service: str) -> None:
     }
     response = requests.post(f"{service}/api/graphs", json=data)
 
-    data = {
+    id = {
         "id": "rm-rf",
     }
-    response = requests.delete(f"{service}/api/graphs", json=data)
+    response = requests.delete(f"{service}/api/graphs", json=id)
     assert response.status_code == 200
-
-    response = requests.get(f"{service}/api/graphs", json=data)
+    response = requests.get(f"{service}/api/graphs", json=id)
     assert response.status_code == 404
     assert response.content.decode("utf-8") == '{"message":"No such graph: \'rm-rf\'"}'
+
+
+@pytest.mark.contract
+def test_get_at_timestamp(service: str) -> None:
+    """Get graph at specific time."""
+    graphs = []
+    graph_id = "d4t3t1m3"
+
+    for i in range(3):
+        data = {
+            "id": graph_id,
+            "graph": f"""
+            @prefix si: <https://www.w3schools.com/rdf/> .
+
+            <https://www.w3schools.com> si:author "Jan Egil Refsnes" ;
+                si:title "W3Schools{i}" .
+            """,
+        }
+        response = requests.post(f"{service}/api/graphs", json=data)
+        assert response.status_code == 200
+
+        graphs.append((data, time.time()))
+        time.sleep(2)
+
+    id = {
+        "id": graph_id,
+    }
+    response = requests.delete(f"{service}/api/graphs", json=id)
+    assert response.status_code == 200
+    response = requests.get(f"{service}/api/graphs", json=id)
+    assert response.status_code == 404
+
+    for _i, (graph, t) in enumerate([*graphs, *graphs[::-1]]):
+        tid = {
+            "id": graph_id,
+            "timestamp": t,
+        }
+        response = requests.get(f"{service}/api/graphs", json=tid)
+        assert response.status_code == 200
+        assert response.content.decode("utf-8") == dedent(graph["graph"]).strip()
+
+    tid = {
+        "id": graph_id,
+        "timestamp": time.time(),
+    }
+    response = requests.get(f"{service}/api/graphs", json=tid)
+    assert response.status_code == 404
+    assert (
+        response.content.decode("utf-8")
+        == f'{{"message":"No such graph: \'{graph_id}\'"}}'
+    )

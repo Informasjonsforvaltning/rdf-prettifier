483ac0a3e60bd1e041a47f0aea98c1130025d1d7
Amund Tenstad <github@amund.io>
2022-10-12 15:30:32 +0200

    feat: serve all the graphs when no id specified

--- rdf_diff_store/rdf.py
+++ rdf_diff_store/rdf.py
@@ -1,6 +1,8 @@
 """RDF."""
 
-from typing import Union
+from typing import Optional, Union
+
+from rdf_diff_store.git import iterate_all_graphs
 
 from rdflib import Graph
 
@@ -9,4 +11,13 @@ def to_turtle(graph: str, format: Union[None, str]) -> str:
     """Convert graph to turtle format."""
     # graph.strip() can fix invalid xml format (leading newline)
     g = Graph().parse(data=graph.strip(), format=format)
+    return g.serialize(format="text/turtle").strip()
+
+
+async def load_all_graphs(timestamp: Optional[int]) -> str:
+    """Load all graphs."""
+    g = Graph()
+    async for graph in iterate_all_graphs(timestamp):
+        g.parse(data=graph)
+
     return g.serialize(format="text/turtle").strip()
--- rdf_diff_store/sparql.py
+++ rdf_diff_store/sparql.py
@@ -4,14 +4,14 @@ from typing import Optional
 
 from rdflib import Graph
 
-from .git import load_all_graphs
+from .git import iterate_all_graphs
 
 
 async def query_sparql(query: str, timestamp: Optional[int] = None) -> str:
     """Query SparQL."""
     g = Graph()
 
-    async for graph in load_all_graphs(timestamp):
+    async for graph in iterate_all_graphs(timestamp):
         g.parse(data=graph)
 
     result = g.query(query).serialize(format="json")

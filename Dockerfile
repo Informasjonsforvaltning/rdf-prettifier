da1052ec09c9d71b41cda9595b33b2832ef70d26
Hege Aalvik <hegeaal@gmail.com>
2023-04-12 09:54:15 +0200

    chore: update deps

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.10 as requirements
 
 WORKDIR /tmp
 
-RUN pip install poetry==1.2.0
+RUN pip install poetry==1.4.2
 COPY ./pyproject.toml ./poetry.lock* ./
 RUN poetry export -f requirements.txt --output requirements.txt --without-hashes
 
--- rdf_prettifier/rdf.py
+++ rdf_prettifier/rdf.py
@@ -9,6 +9,8 @@ def prettify(
     graph: str, input_format: Union[None, str], output_format: Union[None, str]
 ) -> str:
     """Convert graph to specified format and prettify."""
+    if output_format is None:
+        output_format = "ttl"
     # graph.strip() can fix invalid xml format (leading newline)
     g = Graph().parse(data=graph.strip(), format=input_format)
     return g.serialize(format=output_format).strip()
